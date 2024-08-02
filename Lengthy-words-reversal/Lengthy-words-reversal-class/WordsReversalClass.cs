67ca2bb91eac87424914c3155eb3dc6d5f71bf1f
Serg_Goncharov <s.goncharov@sh.dunice.net>
2024-08-02 17:55:54 +0300

    Added wordsReversedClass and reversewords method

---
+++ Lengthy-words-reversal/Lengthy-words-reversal-class/WordsReversalClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lengthy_words_reversal.Lengthy_words_reversal_class
+{
+    internal static class WordsReversalClass
+    {
+        public static string ReversWords(string? innerStr) 
+        {
+
+            if (innerStr?.Length < 5 || string.IsNullOrEmpty(innerStr) || string.IsNullOrWhiteSpace(innerStr))
+            {
+                return innerStr ?? string.Empty;
+            }
+
+            string outer = innerStr.TrimEnd().TrimStart();
+
+            string[] words = outer.Split(' ');
+
+            outer = string.Empty;
+
+            foreach (string word in words)
+            {
+                if (word.Length < 5)
+                {
+                    outer += " " + word;
+                }
+                else 
+                {
+                    char[] chars = word.ToCharArray();
+                    Array.Reverse(chars);
+                    outer += " " + new string(chars);
+                }
+            }
+
+            return outer;
+        }
+    }
+}
--- Lengthy-words-reversal/Program.cs
+++ Lengthy-words-reversal/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using Lengthy_words_reversal.Lengthy_words_reversal_class;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(WordsReversalClass.ReversWords("     vesr    "));
+        Console.WriteLine(WordsReversalClass.ReversWords("     reversed    "));
+        Console.WriteLine(WordsReversalClass.ReversWords("               "));
+        Console.WriteLine(WordsReversalClass.ReversWords(null));
+        Console.WriteLine(WordsReversalClass.ReversWords("reversed not reversed not reversed"));
+
+        Console.ReadKey();
+    }
+}
